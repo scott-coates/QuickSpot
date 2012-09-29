70691e83f32668f795b899458bd88fffb5adae0f
Scott Coates <scott.c.coates@gmail.com>
2012-09-28 20:22:38 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+QuickSpot::Application.config.secret_token = 'f840a7a39d1d0941582bc46cb2954f408b5d4b4d4b808dba6494c3fc62145b315915fe7fa57e17ca5f4b2e2690d9f7464055001089452c48d01bd230e3b0b081'
