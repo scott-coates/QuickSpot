d4338dabbd7610a627a0dc43c20ffc6335168229
Scott Coates <scott.c.coates@gmail.com>
2012-09-29 19:03:12 -0700

    added transition to explanation

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,5 +1,13 @@
 $(function(){
-   $("#explain-site-button").click(function(){
-        $('.site-intro-explanation').transition({ x: -1000 }, 500, 'ease');
-    }); 
+	$("#explain-site-button").click(function(){
+		$('#intro').transition({ x: -330, opacity:0, rotate: 30}, 500, 'ease',function () {
+			this.hide();
+			$('#intro-background').transition({ opacity:0}, 500, 'ease',function() {
+				this.hide();
+				$("#explanation-background")
+				.show()
+				.css({opacity:1});
+			});
+		});
+	});
 });
